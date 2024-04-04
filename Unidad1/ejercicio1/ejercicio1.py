963112b232164f04cbeda33405668916e2396c3a
tomy <tomas.navarro200013@gmail.com>
2024-04-04 18:24:32 -0300

    git(feat): add folders

---
+++ Unidad1/ejercicio1/ejercicio1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+
+"""
+Elija una de las imágenes color que tomó para la clase y aplique separación de canales y elija un método para transformarla en escala de grises. 
+Muestre por pantalla los resultados obtenidos. 
+"""
+
+#def channel_separator(* img_path:str, )
